3158069e55e05d5bd5c6aebaa0bd27ff0528c6a7
Dilip Thomas <dthomas84@gmail.com>
2019-04-14 10:11:15 +1000

    make jest and eslint play nicely together

--- gilded-rose.test.js
+++ gilded-rose.test.js
@@ -1,8 +1,9 @@
-const {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+const { Shop, Item } = require('./gilded-rose.js');
+
+describe('Gilded Rose', () => {
+  test('should store Items', () => {
+    const gildedRose = new Shop([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual('foo');
   });
 });
