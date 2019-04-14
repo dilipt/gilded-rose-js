455552180c2d8476dbecaaf7dc5a70e4aa21947a
Dilip Thomas <dthomas84@gmail.com>
2019-04-14 13:54:10 +1000

    refactored item types to be hidden from the gilded-rose user

--- src/gilded-rose.js
+++ src/gilded-rose.js
@@ -1,9 +1,26 @@
+const {
+  AgedBrie,
+  RandomItem,
+  BackstagePass,
+  LegendaryItem,
+  ConjuredItem,
+} = require('./item');
+
 class Shop {
   constructor(items = []) {
     this.items = items;
   }
 
   updateQuality() {
+    this.items = this.items.map((item) => {
+      switch (item.name) {
+        case 'Sulfuras, Hand of Ragnaros': return new LegendaryItem();
+        case 'Aged Brie': return new AgedBrie(item.sellIn, item.quality);
+        case 'Backstage passes to a TAFKAL80ETC concert': return new BackstagePass(item.sellIn, item.quality);
+        case 'Conjured Item': return new ConjuredItem(item.name, item.sellIn, item.quality);
+        default: return new RandomItem(item.name, item.sellIn, item.quality);
+      }
+    });
     this.items.forEach(item => item.update());
     return this.items;
   }
--- src/gilded-rose.test.js
+++ src/gilded-rose.test.js
@@ -1,11 +1,15 @@
 const { Shop } = require('./gilded-rose');
-const {
-  RandomItem, LegendaryItem, AgedBrie, BackstagePass, ConjuredItem,
-} = require('./item');
+const { Item } = require('./item');
+
+const Specials = {
+  Brie: 'Aged Brie',
+  Pass: 'Backstage passes to a TAFKAL80ETC concert',
+  Lego: 'Sulfuras, Hand of Ragnaros',
+};
 
 describe('Gilded Rose', () => {
   test('should degrade a random Item', () => {
-    const gildedRose = new Shop([new RandomItem('foo', 5, 5)]);
+    const gildedRose = new Shop([new Item('foo', 5, 5)]);
     const items = gildedRose.updateQuality();
     const fooItem = items.shift();
     expect(fooItem.name).toEqual('foo');
@@ -13,8 +17,16 @@ describe('Gilded Rose', () => {
     expect(fooItem.quality).toEqual(4);
   });
 
+  test('should decrease quality of Item by 2 when past use-by date', () => {
+    const GildedRose = new Shop([new Item('foo', 0, 5)]);
+    const items = GildedRose.updateQuality();
+    const item = items.shift();
+    expect(item.quality).toEqual(3);
+    expect(item.sellIn).toEqual(-1);
+  });
+
   test('should not degrade Item quality below zero', () => {
-    const gildedRose = new Shop([new RandomItem('foo', 0, 0)]);
+    const gildedRose = new Shop([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
     const fooItem = items.shift();
     expect(fooItem.name).toEqual('foo');
@@ -23,61 +35,61 @@ describe('Gilded Rose', () => {
   });
 
   test('should not degrade Hand of Rag', () => {
-    const GildedRose = new Shop([new LegendaryItem()]);
+    const GildedRose = new Shop([new Item(Specials.Lego, 0, 0)]);
     const items = GildedRose.updateQuality();
     expect(items[0].quality).toEqual(80);
   });
 
-  test('should increase quality of Aged Brie', () => {
-    const GildedRose = new Shop([new AgedBrie(3, 1)]);
+  test('should increase Aged Brie quality', () => {
+    const GildedRose = new Shop([new Item(Specials.Brie, 3, 1)]);
     const items = GildedRose.updateQuality();
     const brie = items.shift();
     expect(brie.sellIn).toEqual(2);
     expect(brie.quality).toEqual(2);
   });
 
-  test('should decrease quality of Item by 2 when past use-by date', () => {
-    const GildedRose = new Shop([new RandomItem('foo', 0, 5)]);
+  test('should increase Aged Brie quality twofold when past use-by date', () => {
+    const GildedRose = new Shop([new Item(Specials.Brie, 0, 2)]);
     const items = GildedRose.updateQuality();
-    const item = items.shift();
-    expect(item.quality).toEqual(3);
-    expect(item.sellIn).toEqual(-1);
+    const brie = items.shift();
+    expect(brie.sellIn).toEqual(-1);
+    expect(brie.quality).toEqual(4);
   });
 
-  test('Backstage Pass should increase in quality by default', () => {
-    const GildedRose = new Shop([new BackstagePass(20, 10)]);
+  test('should increase Backstage Pass quality by default', () => {
+    const GildedRose = new Shop([new Item(Specials.Pass, 20, 10)]);
     const items = GildedRose.updateQuality();
     const item = items.shift();
     expect(item.sellIn).toEqual(19);
     expect(item.quality).toEqual(11);
   });
 
-  test('Backstage Pass should increase quality by 2 within 10 days', () => {
-    const GildedRose = new Shop([new BackstagePass(10, 11)]);
+  test('should increase Backstage Pass quality by 2 within 10 days', () => {
+    const GildedRose = new Shop([new Item(Specials.Pass, 10, 11)]);
     const items = GildedRose.updateQuality();
     const pass = items.shift();
     expect(pass.sellIn).toEqual(9);
     expect(pass.quality).toEqual(13);
   });
 
-  test('Backstage Pass should increase quality by 3 within 5 days', () => {
-    const GildedRose = new Shop([new BackstagePass(5, 20)]);
+  test('should increase Backstage Pass quality by 3 within 5 days', () => {
+    const GildedRose = new Shop([new Item(Specials.Pass, 5, 20)]);
     const items = GildedRose.updateQuality();
     const pass = items.shift();
     expect(pass.sellIn).toEqual(4);
     expect(pass.quality).toEqual(23);
   });
 
-  test('Conjured Items decrease in quality twice as quickly', () => {
-    const GildedRose = new Shop([new ConjuredItem('mana cakes', 5, 10)]);
+  test('should decrease Conjured Items quality twice as quickly', () => {
+    const GildedRose = new Shop([new Item('Conjured Item', 5, 10, true)]);
     const items = GildedRose.updateQuality();
     const cake = items.shift();
     expect(cake.quality).toEqual(8);
     expect(cake.sellIn).toEqual(4);
   });
 
-  test('Conjured items decrease even further in quality after use-by date', () => {
-    const GildedRose = new Shop([new ConjuredItem('mana', 0, 10)]);
+  test('should decrease Conjured items even further in quality after use-by date', () => {
+    const GildedRose = new Shop([new Item('Conjured Item', 0, 10)]);
     const items = GildedRose.updateQuality();
     const mana = items.shift();
     expect(mana.sellIn).toEqual(-1);
